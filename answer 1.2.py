881e135d81905a885e5c5757f67b5cb6ff132e38
Kalumba Mung'omba <58848732+ernestkalji2@users.noreply.github.com>
2023-06-05 12:39:46 +0200

    answer 1.2.py

---
+++ answer 1.2.py
@@ -0,0 +1,44 @@
+class MobilePhone:
+    def __init__(self, screen_type, network_type, dual_sim, front_camera, rear_camera, ram, storage):
+        self.screen_type = screen_type
+        self.network_type = network_type
+        self.dual_sim = dual_sim
+        self.front_camera = front_camera
+        self.rear_camera = rear_camera
+        self.ram = ram
+        self.storage = storage
+
+    def make_call(self, number):
+        print("Calling", number)
+
+    def receive_call(self):
+        print("Receiving a call")
+
+    def take_a_picture(self):
+        print("Taking a picture")
+
+
+class Apple(MobilePhone):
+    def __init__(self, screen_type, network_type, dual_sim, front_camera, rear_camera, ram, storage):
+        super().__init__(screen_type, network_type, dual_sim, front_camera, rear_camera, ram, storage)
+
+
+class Samsung(MobilePhone):
+    def __init__(self, screen_type, network_type, dual_sim, front_camera, rear_camera, ram, storage):
+        super().__init__(screen_type, network_type, dual_sim, front_camera, rear_camera, ram, storage)
+
+
+# Creating objects of Apple class with different properties
+iphone_12 = Apple("Touch Screen", "5G", False, "12MP", "48MP", "4GB", "64GB")
+iphone_11 = Apple("Touch Screen", "4G", True, "8MP", "32MP", "3GB", "32GB")
+iphone_x = Apple("Touch Screen", "4G", True, "12MP", "32MP", "4GB", "64GB")
+
+# Creating objects of Samsung class with different properties
+galaxy_s21 = Samsung("Touch Screen", "5G", True, "16MP", "64MP", "8GB", "128GB")
+galaxy_s20 = Samsung("Touch Screen", "5G", False, "12MP", "48MP", "6GB", "256GB")
+galaxy_a71 = Samsung("Touch Screen", "4G", True, "12MP", "32MP", "4GB", "128GB")
+
+# Using the mobile phone functionalities
+iphone_12.make_call("1234567890")
+galaxy_s21.receive_call()
+iphone_x.take_a_picture()
